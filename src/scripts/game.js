b490b604bdf2a004dee7c037cae358b69f0a2b98
Saulo Klein Nery <saulokn@outlook.com>
2022-11-13 00:33:08 -0300

    feat(gameLoop and basicSprites): Create gameLoop and add the sprites of ground, flappybird and background to the page

---
+++ src/scripts/game.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sprites = new Image();
+    sprites.src = "../assets/imgs/sprites.png";
+
+    const canvas = document.querySelector("canvas");
+    const context = canvas.getContext("2d");
+
+    const backgroundColor = "#70c5ce";
+
+    const draw = (object) => {
+        context.drawImage(
+            sprites,
+            object.sourceX,
+            object.sourceY,
+            object.width,
+            object.height,
+            object.x,
+            object.y,
+            object.width,
+            object.height
+        );
+
+        if (object.isDouble) {
+            context.drawImage(
+                sprites,
+                object.sourceX,
+                object.sourceY,
+                object.width,
+                object.height,
+                object.newPositionX,
+                object.y,
+                object.width,
+                object.height
+            );
+        }
+    };
+
+    const flappyBird = {
+        sourceX: 0,
+        sourceY: 0,
+        width: 34,
+        height: 24,
+        x: 0,
+        y: 0,
+        draw() {
+            draw(this);
+        },
+    };
+
+    const ground = {
+        sourceX: 0,
+        sourceY: 609,
+        width: 224,
+        height: 113,
+        x: 0,
+        y: canvas.height - 113,
+        isDouble: true,
+        newPositionX: 224,
+        draw() {
+            draw(this);
+        },
+    };
+
+    const background = {
+        sourceX: 390,
+        sourceY: 3,
+        width: 275,
+        height: 204,
+        x: 0,
+        y: canvas.height - 200 - 113,
+        isDouble: true,
+        newPositionX: 275,
+        draw() {
+            context.fillStyle = backgroundColor;
+            context.fillRect(0, 0, canvas.width, canvas.height);
+
+            draw(this);
+        },
+    };
+
+    function gameLoop() {
+        background.draw();
+        flappyBird.draw();
+        ground.draw();
+        requestAnimationFrame(gameLoop);
+    }
+
+    gameLoop();
+});
